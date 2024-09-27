111619fbf3bfb5dd7309df873d7ea68b55386ac2
Welt-liu <1994524450@qq.com>
2024-09-27 09:17:47 +0800

    控制开原模型

--- example/FSUS_control.py
+++ example/FSUS_control.py
@@ -1,4 +1,7 @@
-from robot import Robot
+# from robot import Robot
+import sys
+sys.path.append("../src")
+sys.path.append("..")
 # from dynamixel import Dynamixel
 import numpy as np
 import time
@@ -6,20 +9,38 @@ import mujoco.viewer
 from simulation.interface import SimulatedRobot
 import threading
 
+# 导入依赖
+import serial
+from uservo import UartServoManager
+
+SERVO_PORT_NAME =  '/dev/ttyUSB0' # 舵机串口号
+SERVO_BAUDRATE = 115200 # 舵机的波特率
+SERVO_ID = 0  # 舵机的ID号
+# 初始化串口
+uart = serial.Serial(port=SERVO_PORT_NAME, baudrate=SERVO_BAUDRATE,\
+					 parity=serial.PARITY_NONE, stopbits=1,\
+					 bytesize=8,timeout=0)
+# 初始化舵机管理器
+uservo = UartServoManager(uart)
+
+# 设置舵机为阻尼模式
+uservo.set_damping(SERVO_ID, 200)
+
+
 def read_leader_position():
     global target_pos
     while True:
-        target_pos = np.array(leader.read_position())
-        target_pos = (target_pos / 2048 - 1) * 3.14
-        target_pos[1] = -target_pos[1]
-        target_pos[3] = -target_pos[3]
-        target_pos[4] = -target_pos[4]
+        target_pos[0] = (-(uservo.query_servo_angle(0))*3.14/180)
+        target_pos[1] = uservo.query_servo_angle(1)*3.14/180
+        target_pos[2] = (-(uservo.query_servo_angle(3)-90)*3.14/180)
+        target_pos[4] = (-(uservo.query_servo_angle(4))*3.14/180)
+
 
 # leader_dynamixel = Dynamixel.Config(baudrate=1_000_000, device_name='/dev/tty.usbmodem57380045631').instantiate()
 # leader = Robot(leader_dynamixel, servo_ids=[1, 2, 3, 6, 7])
 # leader.set_trigger_torque()
 
-m = mujoco.MjModel.from_xml_path('simulation/low_cost_robot/scene.xml')
+m = mujoco.MjModel.from_xml_path('../simulation/low_cost_robot/scene.xml')
 d = mujoco.MjData(m)
 
 r = SimulatedRobot(m, d)
@@ -27,9 +48,9 @@ r = SimulatedRobot(m, d)
 target_pos = np.zeros(5)
 
 # Start the thread for reading leader position
-# leader_thread = threading.Thread(target=read_leader_position)
-# leader_thread.daemon = True
-# leader_thread.start()
+leader_thread = threading.Thread(target=read_leader_position)
+leader_thread.daemon = True
+leader_thread.start()
 
 with mujoco.viewer.launch_passive(m, d) as viewer:
     start = time.time()
